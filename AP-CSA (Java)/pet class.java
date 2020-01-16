0e2f669a21d5bbd9b4a08f2ebd5a6d03d546a4c5
Andrew Simonson <asimonson1125@gmail.com>
2020-01-16 10:09:51 -0500

    Create pet class.java

---
+++ AP-CSA (Java)/pet class.java
@@ -0,0 +1,78 @@
+//This was for runestone
+public class TesterClass
+{
+   // main method for testing
+   public static void main(String[] args)
+   {
+      // Create Pet objects and test all your methods
+       Pet p1 = new Pet();
+       Pet p1andahalf = new Pet("E.", 7, "unknown, possibly satanic", -69, "new breed, we named it 'richardDog'.");
+       Pet p2 = new Pet("Gregory", 4, "dog creature", 69000.4, "Hefty Retriever");
+       System.out.println(p2.gimmeName() + ", " + p2.gimmeType() + ", " + p2.gimmeBreed());
+       System.out.println(p2.gimmeAge() + " years old and weighs a hefty " + p2.gimmeWeight() + " lbs.");
+       System.out.println();
+       System.out.println();
+       System.out.println(p1andahalf.toString());
+       System.out.println();
+       System.out.println();
+       System.out.println(p2.toString());
+
+   }
+ }
+
+/**
+    Pet class (complete comments)
+    @author
+    @since
+
+*/
+class Pet
+{
+   // complete class definition
+    private String name, type, breed;
+    private int age;
+    private double weight;
+    
+    public Pet(){
+        name  = "unnamed";
+        age = -1;
+        type = "unknown type";
+        weight = -1;
+        breed = "unknown breed";
+    }
+    
+    public Pet(String initName, int initAge, String initType, double initWeight, String initBreed){
+        name = initName;
+        age = initAge;
+        type = initType;
+        weight = initWeight;
+        breed = initBreed;
+    }
+    
+    public String gimmeName(){
+        return(name);
+    }
+    
+    public int gimmeAge(){
+        return(age);
+    }
+    
+    public String gimmeType(){
+        return(type);
+    }
+    
+    public double gimmeWeight(){
+        return(weight);
+    }
+    
+    public String gimmeBreed(){
+        return(breed);
+    }
+    
+    public String toString(){
+        //e
+        return(name + ":  " + " This " + breed + " " + type + " is " + age + " years old and weighs " + weight + " lbs.");
+    }
+    
+}
+
