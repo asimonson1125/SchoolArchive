1004f0c08981b0ca8490138631d5d3ebb9ad54d4
Andrew Simonson <asimonson1125@gmail.com>
2020-01-24 09:50:39 -0500

    failsafe for safety
    put failsafe on buyer so it never again fucks with the crate crate

--- DOM automation/buyArmorCrates.js
+++ DOM automation/buyArmorCrates.js
@@ -1,6 +1,15 @@
 var intended = 100; //set to max packs to buy
 var packnum = 4; //1 for infantry, 2 for rare infantry, 3 for epic infantry, 4 for armor, 7 for air, 10 for Special Forces, 13 for static.
-//16 for 'crate'. It used up all of my loot tokens.  Not a good idea.
+//16 for 'crate'. It used up all of my loot tokens.  Not a good idea. 
+//let's not make that mistake again.
+if(packnum > 15){
+  //stop right now you crazy man
+  //like no
+  //just don't try it
+  //I said stop
+  window.location.href = "https://app.nationsgame.net";
+  //I tried to warn you, now you made me stop you by force
+}
 
 function sleep(ms) { //make wait function
   return new Promise(resolve => setTimeout(resolve, ms));
