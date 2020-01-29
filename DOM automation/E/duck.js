9aa1d5e5c65dfcbce32c9fc791c6e1b0d7bdbfdb
Andrew Simonson <asimonson1125@gmail.com>
2020-01-28 20:48:09 -0500

    Create duck.js

---
+++ DOM automation/E/duck.js
@@ -0,0 +1,3 @@
+//why did I make this again?
+//well here it is until I add more to this folder I guess
+window.location.replace('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthenypost.files.wordpress.com%2F2017%2F06%2Fduck.jpg%3Fquality%3D90%26strip%3Dall%26w%3D1200&f=1&nofb=1');
