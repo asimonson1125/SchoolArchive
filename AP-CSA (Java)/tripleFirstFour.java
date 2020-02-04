35c232d3fbe866d502d8effb3429de49c02acd03
Andrew Simonson <asimonson1125@gmail.com>
2020-02-04 10:14:27 -0500

    Create tripleFirstFour.java

---
+++ AP-CSA (Java)/tripleFirstFour.java
@@ -0,0 +1,44 @@
+//for runestone 6.1 
+
+
+public class ArrayWorker
+{
+   private int[ ] values;
+
+   public ArrayWorker(int[] theValues)
+   {
+      values = theValues;
+   }
+
+   /** Doubles the first 5 elements of the array */
+   public void doubleFirstFive()
+   {
+     // Notice: && i < 5
+     for (int i = 0; i < values.length && i < 5; i++)
+     {
+       values[i] = values[i] * 2;
+     }
+   }
+   public void tripleFirstFour(){
+       for(int i = 0; i< values.length && i <4; i++){
+           values[i] = values[i]*3;
+       }
+   }
+
+   public void printArray()
+   {
+     for (int i = 0; i < values.length; i++)
+      {
+        System.out.println(  values[i] );
+      }
+   }
+
+   public static void main(String[] args)
+   {
+     int[] numArray = {3, 8, -3, 2, 20, 5, 33, 1};
+     ArrayWorker worker = new ArrayWorker(numArray);
+     worker.tripleFirstFour();
+     worker.printArray();
+   }
+}
+
