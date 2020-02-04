c4f1db0aa7ac0d7a9c5d9b826e5ab205f069308b
Andrew Simonson <asimonson1125@gmail.com>
2020-02-04 09:57:03 -0500

    Create arrays.java

---
+++ AP-CSA (Java)/arrays.java
@@ -0,0 +1,20 @@
+//for runestone 6.1
+
+public class Test1
+{
+   public static void main(String[] args)
+   {
+     // Array example
+     int[] highScores = new int[10];
+     double[] prices = new double[5];
+     String[] names = new String[5];
+     // Add an array of 5 doubles called prices.
+
+     // Add an array of 5 Strings called names.
+
+     System.out.println("Array highScores declared with size " + highScores.length);
+       System.out.println("Array prices declared with size " + prices.length);
+       System.out.println("Array names declared with size " + names.length);
+   }
+}
+
