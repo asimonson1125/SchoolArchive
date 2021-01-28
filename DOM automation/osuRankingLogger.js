6e6b44e13de23e98c669f5b70d2043a2517cd1c0
Andrew Simonson <asimonson1125@gmail.com>
2021-01-27 22:49:21 -0500

    Update osuRankingLogger.js
    documentation lol

--- DOM automation/osuRankingLogger.js
+++ DOM automation/osuRankingLogger.js
@@ -1,4 +1,5 @@
 // Made this to keep track of my ranking progression in Osu before the graph on your profile is deleted (after 90 days)
+// Just go to the target's profile and run in console and it'll spit out all the rankings in the chart (chronologically)
 
 // Do whatever with it lol
 
